d6db1e751725ede3b3f79631bd8ae6239e3a307a
Lorenzo Calisti <djonline96@gmail.com>
2021-12-24 12:07:54 +0100

    Add .gitignore and basic interface to ts.erl

--- ts.erl
+++ ts.erl
@@ -1,1 +1,23 @@
 -module(ts).
+
+-export([new/1, in/2, in/3, rd/2, rd/3, out/2, addNode/2, removeNode/2, nodes/1]).
+
+% Node management
+addNode(TS, Node) -> ok.
+removeNode(TS, Node) -> ok.
+nodes(TS) -> ok.
+
+% Tuple Space management
+new(Name) -> ok.
+
+in(TS, Pattern) -> ok.
+in(TS, Pattern, Timeout) -> ok.
+
+rd(TS, Pattern) -> ok.
+rd(TS, Pattern, Timeout) -> ok.
+
+out(TS, Tuple) -> ok.
+
+match(L1, L2) -> ok.
+
+tuple2list(Tuple) -> [].
