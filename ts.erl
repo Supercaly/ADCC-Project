ca6eaa6862080e791605d704d4a8d9beadf1265c
Lorenzo Calisti <djonline96@gmail.com>
2021-12-27 19:13:40 +0100

    Write the new function and part of the main loop

--- ts.erl
+++ ts.erl
@@ -1,23 +1,55 @@
 -module(ts).
 
--export([new/1, in/2, in/3, rd/2, rd/3, out/2, addNode/2, removeNode/2, nodes/1]).
+-export([init/1, new/1, out/2, match/2]).
 
 % Node management
-addNode(TS, Node) -> ok.
-removeNode(TS, Node) -> ok.
-nodes(TS) -> ok.
+% addNode(TS, Node) -> ok.
+
+% removeNode(TS, Node) -> ok.
+
+% nodes(TS) -> ok.
 
 % Tuple Space management
-new(Name) -> ok.
+new(Name) ->
+    register(me, self()),
+    io:format("Create new tuple space \"~p\"\n", [Name]),
+    register(Name, spawn(?MODULE, init, [[]])),
+    {ok}.
 
-in(TS, Pattern) -> ok.
-in(TS, Pattern, Timeout) -> ok.
+init(Data) ->
+    me ! {all_data, Data},
+    receive
+      % Message for in operation
+      {ts_in, Pid, Pattern} -> 
+        case lists:filter(fun (Tuple)-> match(Pattern, Tuple) end, Data) of
+          [] -> init(Data);
+          [Element | _] -> 
+            Pid!{ok, Element},
+            NewData = lists:delete(Element, Data),
+            init(NewData)
+        end;
+      % Message for out operation
+      {ts_out, Tuple} ->
+	      NewData = Data ++ [Tuple],
+	      me ! {"Got out for tuple", Tuple},
+	      init(NewData);
+      % Unknown message
+      _Msg -> 
+        me ! {unknown, _Msg}, 
+        init(Data)
+    end.
 
-rd(TS, Pattern) -> ok.
-rd(TS, Pattern, Timeout) -> ok.
+% in(TS, Pattern) -> ok.
 
-out(TS, Tuple) -> ok.
+% in(TS, Pattern, Timeout) -> ok.
 
-match(L1, L2) -> ok.
+% rd(TS, Pattern) -> ok.
 
-tuple2list(Tuple) -> [].
+% rd(TS, Pattern, Timeout) -> ok.
+
+out(TS, Tuple) -> TS ! {ts_out, Tuple}.
+
+match(L1, L2) -> L1 == L2.
+
+%tuple2list(Tuple) -> [].
+
