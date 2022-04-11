4e51446557f7fd4a409064dbc34dfffee288f66c
Lorenzo Calisti <djonline96@gmail.com>
2022-04-11 20:36:50 +0200

    Add implementation of matrix problem

--- apps/experiments/src/experiments.erl
+++ apps/experiments/src/experiments.erl
@@ -8,16 +8,16 @@ usage(Io) ->
     io:format(Io, "    -h           print this help message:~n", []),
     io:format(Io, "~n", []),
     io:format(Io, "  SUBCOMMAND:~n", []),
-    io:format(Io, "    pwd          run the 'password search' case study~n", []),
-    io:format(Io, "    matrix       run the 'matrix multiplication' case study~n", []),
+    io:format(Io, "    pwd <n-worker> <n-pwd>               run the 'password search' case study~n", []),
+    io:format(Io, "    matrix <n-worker> <matrix-size>      run the 'matrix multiplication' case study~n", []),
     io:format(Io, "~n", []).
 
 main(["pwd", NMasters, NWorkers, NPwds]) ->
     pwd_search:pwd_search_task(list_to_integer(NMasters), 
         list_to_integer(NWorkers), 
         list_to_integer(NPwds));    
-main(["matrix"]) ->
-    io:format("TODO: Matrix multiplication step is not implemented yet");    
+main(["matrix", NWorkers, Size]) ->
+    matrices:matrix_multiplication_task(list_to_integer(NWorkers), list_to_integer(Size));
 main(["-h"]) ->
     usage(standard_io);
 main(_) ->
--- apps/experiments/src/matrices.erl
+++ apps/experiments/src/matrices.erl
@@ -1,128 +1,145 @@
 -module(matrices).
 
-%TODO
 -export([
-    % pwd_search_task/3,
-    % init_spaces/0,
-    % populate_pwd/1,
-    % master_task/1,
-    % worker_task/0
+    matrix_multiplication_task/2
 ]).
 
+% Perform the matrix multiplication test case
+% NOTE: This code is run by the supervisor node
+matrix_multiplication_task(NWorkers, MatrixSize) ->
+    nodelib:init_sup(),
+    {[Master], Workers} = nodelib:spawn_nodes(1, NWorkers),
+    lists:foreach(fun(N) ->
+        ok = rpc:call(N, proflib, start, ["./profiler/matrix/"])
+    end, [Master]++Workers),
 
+    init_space(),
+    
+    nodelib:run_on_node(Master, fun() ->
+        proflib:begine(task),
+        master_task(MatrixSize),
+        proflib:ende(task)
+    end),
 
-% Initialize the needed spaces
-init_spaces() ->
-    ok = ts:new(pwd_space),
-    ok = ts:new(task_space),
     lists:foreach(fun(Node) ->
-        ok = ts:addNode(pwd_space, Node),
-        ok = ts:addNode(task_space, Node)
+        nodelib:run_on_node(Node, fun() ->
+            proflib:begine(task),
+            worker_task(trunc(math:sqrt(MatrixSize))),
+            proflib:ende(task)
+        end)
+    end, Workers),
+
+    nodelib:wait_for_master(),
+    ok.
+
+% Initialize the needed space
+init_space() ->
+    ok = ts:new(matrix_space),
+    lists:foreach(fun(Node) ->
+        ok = ts:addNode(matrix_space, Node)
     end, nodes()).
 
+% Create a squared matrix of given size 
+% populated with random integer values
+generate_matrix(Size) -> 
+    [rand:uniform(10) || _ <- lists:seq(1,Size)].
 
+% Split a squared matrix by rows and return a list
+% of each rows
+split_matrix_by_row(Matrix, Size) ->
+    RowLen = trunc(math:sqrt(Size)),
+    [lists:sublist(Matrix, X, RowLen) || X <- lists:seq(1,Size,RowLen)].
 
 % Task run by the master node
 % NOTE: This code is run by the master node
-master_task(Size) -> 
-
-    MatrixA = generateMatrix(Size),
-    MatrixB = generateMatrix(Size),
-
-    % the master creates, in a single cicle, each element of the matrix A
-    % and each row of matric B
-
+master_task(MatrixSize) -> 
+    % generate two matrices
+    MatrixA = generate_matrix(MatrixSize),
+    MatrixB = generate_matrix(MatrixSize),
+    io:format("Matrix A:~n"),
+    print_matrix(MatrixA, MatrixSize),
+    io:format("Matrix B:~n"),
+    print_matrix(MatrixB, MatrixSize),
+    
+    % send each element of matrix A as a message
     lists:foldl(fun(Elem, Index) ->
-        ts:out({matrixA, Index, Elem}),
+        ts:out(matrix_space, {matrixA, Index, Elem}),
         Index + 1
     end, 0, MatrixA),
 
-    lists:foldl(fun(Elem, Index) ->
-        ts:out({matrixB, Index, Elem}),
-        Index + 1
-    end, 0, MatrixB),
-
-    lists:foreach(fun(Hash) ->
-        proflib:begine(write_task),
-        ok = ts:out(task_space, {search_task, Hash}),
-        proflib:ende(write_task)
-    end, Hashes),
-
+    % send each line of matrix B as a message
+    % Note: each line must be sent n-rows times since it's required
+    % more than once
+    MatrixBRows = split_matrix_by_row(MatrixB, MatrixSize),
+    lists:foreach(fun(Rep) ->
+        lists:foldl(fun(Row, Index) ->
+            ts:out(matrix_space, {matrixB, Index, Row, Rep}),
+            Index+1
+        end, 0, MatrixBRows)
+    end, lists:seq(1, trunc(math:sqrt(MatrixSize)))),
+    
     io:format("Node '~p' has sent all his requests~n", [node()]),
+    
+    % wait for the workers to respond with matrix C
+    Res = wait_for_matrix([], MatrixSize),
+    io:format("Matrix C:~n"),
+    print_matrix(Res, MatrixSize),
 
-    % wait for worker to respond with passwords
-    wait_for_passwords(length(Hashes)),
+    % Tell the supervisor the task is complete
+    case nodelib:get_supervisor() of
+        undefined -> exit({supervisor_undefined});
+        Pid -> Pid!{finished, node()}
+    end,
     ok.
 
 % Task run by the worker node
 % NOTE: This code is run by the worker node
-worker_task() ->
-    % wait for new hash to search
-    {ok, {search_task, Hash}} = ts:in(task_space,{search_task, any}),
-    
-    % find the pwd for given hash in the pwd_space
-    proflib:begine(read_pwd),
-    {ok, {Pwd, _Hash}} = ts:rd(pwd_space, {any,Hash}),
-    proflib:ende(read_pwd),
-    
-    % respond with the found password
-    proflib:begine(write_pwd),
-    ok = ts:out(task_space, {found_password, Hash, Pwd}),
-    proflib:ende(write_pwd),
-    
-    % search next hash
-    worker_task(),
+worker_task(RowSize) ->
+    % wait for one element of matrix A
+    {ok, {_, Idx, ValA}} = ts:in(matrix_space, {matrixA, any, any}),
+   
+    % wait for the corresponding row of matrix B
+    {ok, {_, _, RowB, _}} = ts:in(matrix_space, {matrixB, Idx rem RowSize, any, any}),
+   
+    % perform partial multiplication
+    RowC = lists:map(fun(E) -> E * ValA end, RowB),
+
+    % send the calculated row of matrix C
+    ts:out(matrix_space, {matrixC, Idx div RowSize, RowC}),
+   
+    worker_task(RowSize),
     ok.
 
+% Wait for all the workers to output thei partial calculation
+% This function takes as parameter the partial matrix and his size
+% and returns a matrix list.
+% Note: Internally the matrix is represented as a list of rows
+wait_for_matrix([], N) -> 
+    Len = trunc(math:sqrt(N)),
+    EmptyMatrix = [[0 || _ <- lists:seq(1,Len)] || _ <- lists:seq(1,Len)],
+    wait_for_matrix(EmptyMatrix, N);
+wait_for_matrix(Result, 0) -> lists:append(Result);
+wait_for_matrix(PartialMatrix, N) ->
+    % wait for a computed row of matrix C
+    {ok, {_, Idx, ValC}} = ts:in(matrix_space, {matrixC, any, any}),
 
-% % Wait for workers to find N passwords then finish the task
-% wait_for_passwords(0) -> 
-%     case nodelib:get_supervisor() of
-%         undefined -> exit({supervisor_undefined});
-%         Pid -> Pid!{finished, node()}
-%     end,
-%     ok;
-% wait_for_passwords(N) ->
-%     {ok, {found_password, _Hash, _Pwd}} = ts:in(task_space, {found_password, any, any}),
-%     wait_for_passwords(N-1).
+    % append the row to the matrix
+    {NewMatrix,_} = lists:mapfoldl(fun(Row, Index) ->
+        if 
+            Idx == Index -> {lists:zipwith(fun(E1,E2) -> E1+E2 end, ValC, Row), Index+1};
+            true -> {Row, Index+1}
+        end
+    end, 0, PartialMatrix),
+        
+    % wait for another row
+    wait_for_matrix(NewMatrix, N-1).
 
-% % Perform the password search test case
-% % NOTE: This code is run by the supervisor node
-% pwd_search_task(NMasters, NWorkers, NPwds) ->
-%     nodelib:init_sup(),
-%     {Masters, Workers} = nodelib:spawn_nodes(NMasters, NWorkers),
-%     lists:foreach(fun(N) ->
-%         ok = rpc:call(N, proflib, start, ["./profiler/pwd/"])
-%     end, Masters++Workers),
-
-%     pwd_search:init_spaces(),
-%     Hashes = pwd_search:populate_pwd(NPwds),
-
-%     % TODO: Figure out the termination with 2 or more masters
-%     lists:foreach(fun(Node) ->
-%         nodelib:run_on_node(Node, fun() ->
-%             proflib:begine(task),
-%             pwd_search:master_task(Hashes),
-%             proflib:ende(task)
-%         end)
-%     end, Masters),
-
-%     lists:foreach(fun(Node) ->
-%         nodelib:run_on_node(Node, fun() ->
-%             proflib:begine(task),
-%             pwd_search:worker_task(),
-%             proflib:ende(task)
-%         end)
-%     end, Workers),
-
-%     nodelib:wait_for_nodes(NMasters),
-%     ok.
-
-% % Populate the database with N passwords and hashes
-% populate_pwd(N) ->
-%     lists:map(fun(PwdInt) -> 
-%         Pwd = integer_to_list(PwdInt),
-%         Hash = create_hash(Pwd),
-%         ok = ts:out(pwd_space, {Pwd, Hash}),
-%         Hash
-%     end, lists:seq(0,N-1)).
+% Print a matrix to standrard output
+print_matrix(Matrix, Size) ->
+    lists:foreach(fun(R) -> 
+        io:format("  "),
+        lists:foreach(fun(E) ->
+            io:format("~p ",[E])
+        end, R),
+        io:format("~n")
+    end, split_matrix_by_row(Matrix, Size)).
