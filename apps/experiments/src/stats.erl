93c071646e9e3fbc47a9d328762459b5c596390c
Lorenzo Calisti <djonline96@gmail.com>
2022-04-15 11:53:20 +0200

    Fix stats to produce only cumulative .csv files

--- apps/experiments/src/stats.erl
+++ apps/experiments/src/stats.erl
@@ -1,51 +1,39 @@
 -module(stats).
 
 -export([
-    process_dir/1,
-    do_format_statistics/1
+    process_dir/1
 ]).
 
 process_dir(Dir) -> 
     {ok, Files} = file:list_dir(Dir),
     Logs = lists:filter(fun(E) -> string:equal(string:right(E, 8), "-log.txt") end, Files),
-    Processed = lists:map(fun(E) -> process_log_file(Dir,E) end, Logs),
-    file:write_file(filename:join([Dir, "stats.txt"]),format_statistics(Processed)).
+    Processed = lists:flatten(lists:map(fun(E) -> process_log_file(Dir,E) end, Logs)),
+    HasError = lists:any(fun ({error,_}) -> true; (_) -> false end, Processed),
+    if 
+        HasError -> io:format("Error processing logs '~p':~n", [Dir]);
+        true -> 
+            FileName = string:concat(string:concat("all_", filename:basename(Dir)), ".csv"),
+            file:write_file(filename:join([Dir, FileName]), format_statistics(Processed))
+    end.
 
 process_log_file(Dir, File) -> 
     Path = filename:join([Dir,File]),
     [Node,_] = string:tokens(File, "@"),
     case parse_file(Path) of
         {error, Reason} -> {error, Reason};
-        Data -> {list_to_atom(Node), lists:map(fun({Key, Val}) ->
-            Vals = lists:map(fun(E) -> binary_to_integer(list_to_binary(E)) end, Val),
-            Mean = lists:foldl(fun(E,A) -> A+E end, 0, Vals) / length(Vals),
-            Std = math:sqrt(lists:foldl(fun(E,A) -> A+((E-Mean)*(E-Mean)) end, 0, Vals) / length(Vals)),
-            {Key, #{mean => Mean, std => Std}}
-        end, Data)}
+        Data -> lists:map(fun({K,V}) -> {list_to_atom(Node), K, V} end, Data)
     end.
 
 parse_file(File) ->
     case file:read_file(File) of
         {ok, Data} -> 
             Lines = lists:filter(fun(E) -> byte_size(E) =/= 0 end, binary:split(Data, <<"\n">>, [global])),
-            MappedLines = lists:map(fun(Line) ->
-                [Event,_,_,Time] = string:tokens(binary_to_list(Line), [$\s]),
-                {Event, Time}
-            end, Lines),
-            maps:to_list(lists:foldl(fun({Key, Val}, Map)->
-                maps:put(list_to_atom(Key), maps:get(list_to_atom(Key), Map, [])++[Val], Map) 
-            end, maps:new(), MappedLines));
+            lists:map(fun(Line) ->
+                [Event, Time] = string:tokens(binary_to_list(Line), [$\s]),
+                {list_to_atom(Event), list_to_integer(Time)}
+            end, Lines);
         _Error -> _Error
     end.
 
 format_statistics(Stats) -> 
-    lists:map(fun stats:do_format_statistics/1, Stats).
-
-do_format_statistics({Name, Stat}) when is_list(Stat) -> 
-    StatFmt = format_statistics(Stat),
-    io_lib:format("~p~n",[Name]) ++ StatFmt;
-do_format_statistics({Name, Stat}) -> 
-    StatFmt = maps:fold(fun(Key, Val, Acc) ->
-        Acc ++ io_lib:format("    ~p: ~p~n",[Key, Val])
-    end, [], Stat),
-    io_lib:format("  ~p~n",[Name]) ++ StatFmt.
+    lists:map(fun({N,K,V}) -> io_lib:format("~p, ~p, ~p~n",[N,K,V]) end, Stats).
